39a8704529ddc4d011e9b13d53a14f683f1931f8
JoshuaHolloway <jhollow6@asu.edu>
2021-04-12 12:28:26 -0500

    added svelte integration

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let name;
+</script>
+
+<div>
+  <h1>HHH Hello {name}!</h1>
+</div>
+
+<style>
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
