5a831eacff03a84d5812e412da6fc65f2adf91da
JoshuaHolloway <jhollow6@asu.edu>
2021-04-29 12:12:15 -0500

    correctly closing side drawer when either the side-drawers close button is clicked or if you click in the window OUTSIDE the side-drawer

--- design/navbar-style/js/side-drawer.js
+++ design/navbar-style/js/side-drawer.js
@@ -1,22 +1,70 @@
-import { element_geometry } from './geometry.js';
+import { element_geometry, viewport_geometry } from './geometry.js';
 
-const hamburger = document.querySelector('#hamburger');
+// ==============================================
+
+const hamburger = document.querySelector('#nav-mobile .hamburger');
 const side_drawer = document.querySelector('#side-drawer');
+const side_drawer_close_button = side_drawer.querySelector('.side-drawer__close-button');
 
-// $side-drawer-width  defined in  _side-drawer.scss
-const sidedrawer = document.querySelector('#side-drawer'); 
-const elem_geom = element_geometry(sidedrawer);
-const sidedrawer_width = elem_geom.w;
+// ==============================================
+
+// Get geometry of side-drawer and window:
+const sidedrawer = document.querySelector('#side-drawer'); // $side-drawer-width  defined in  _side-drawer.scss 
+const sidedrawer_width = element_geometry(sidedrawer).w;
+const {viewport_width} = viewport_geometry();
+
+// ==============================================
+
+// ==============================================
+
+const open_side_drawer = () => {
+
+  tween = gsap.to(side_drawer, {
+    x: -sidedrawer_width,
+    onComplete: () => {
+      console.log('animation complete');
+
+      window.addEventListener('click', (event) => {
+        console.log('event.clientX: ', event.clientX, ',  event.clientY: ', event.clientY, ',  viewport_width - sidedrawer_width: ', viewport_width - sidedrawer_width);
+
+        if (event.clientX < viewport_width - sidedrawer_width) {
+          console.log('clicked outside the side-drawer!');
+          close_side_drawer();
+        }
+      });
+
+    }, // onComplete()
+    onReverseComplete: () => {
+      console.log('onReverseComplete()');
+    }, // onReverse()
+  });
+
+}; // open_side_drawer()
+
+const close_side_drawer = () => {
+  tween.reverse();
+}; // close_side_drawer()
+
+// ==============================================
 
 let count = 0;
 // let tween =  gsap.from(side_drawer, {x: 200, paused: true});
-let tween;
-window.addEventListener('click', () => {
-  
-  if (count === 0)
-    tween = gsap.to(side_drawer, {x: -sidedrawer_width});
-  else
-    tween.reverse();
+let tween = gsap.timeline();
+hamburger.addEventListener('click', () => {
 
-  count = (count + 1) % 2;
+  console.log('open nav-drawer');
+  open_side_drawer();
 });
+
+// ==============================================
+
+side_drawer_close_button.addEventListener('click', () => {
+
+  console.log('close nav-drawer');
+  close_side_drawer();
+
+});
+
+
+
+// ==============================================
