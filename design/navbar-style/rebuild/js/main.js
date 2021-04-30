1d907b7a56eebdbeece0cd4a6a9a3d7124c9396e
JoshuaHolloway <jhollow6@asu.edu>
2021-04-30 12:54:00 -0500

    overlay animation

--- design/navbar-style/rebuild/js/main.js
+++ design/navbar-style/rebuild/js/main.js
@@ -1,33 +1,55 @@
 console.log('hi');
 
-const nav__hamburger  = document.querySelector('.nav__hamburger');
-const blur_container = document.querySelector('.blur-container');
 
 let count = 0;
 let master_timeline;
+const duration = 0.5;
 
 // ==============================================
 
+const blur_container = document.querySelector('.blur-container');
 const blur_background = () => {
+
   const timeline = gsap.timeline();
-  
+
   timeline.to(blur_container, {
+    duration,
     filter: 'blur(4px)',
   }); // .to()
-
+  
   return timeline;
 };
 
 // ==============================================
 
-nav__hamburger.addEventListener('click', () => {
+const overlay = document.querySelector('.overlay');
+const translucent_overlay = () => {
 
+  const timeline = gsap.timeline();
+  
+  timeline.to(overlay, {
+    duration,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    onStart:           () => overlay.style.zIndex = 1,
+    onReverseComplete: () => overlay.style.zIndex = -1,
+  }); // .to()
+
+  return timeline;
+};
+
+
+// ==============================================
+
+const nav__hamburger  = document.querySelector('.nav__hamburger');
+window.addEventListener('click', () => {
+  
   console.log('clicked button');
   
   if (count === 0) {
     
     master_timeline = gsap.timeline();
     master_timeline.add( blur_background() );
+    master_timeline.add( translucent_overlay(), '<' );
 
   }
   else {
