e1beaa58eac6bf3c210ef9f23b32f5e2b0b3e526
JoshuaHolloway <jhollow6@asu.edu>
2021-05-02 15:16:44 -0500

    rendering dynamic data for first nav-item

--- design/main/js/nav-dropdown.js
+++ design/main/js/nav-dropdown.js
@@ -48,33 +48,6 @@ const translucent_overlay = () => {
 
 // ==============================================
 
-const drodown_data = {
-  'new_releases': [
-    [
-      'New & Featured',
-      'Shop All New Arrivals',
-      'SNKRS Launch Calendar',
-      'Best of Air Max',
-      'Member Access',
-      'Top Picks for Mom',
-      'Mix & Match with Mom'
-    ],
-    [],
-  ],
-  'men': [],
-  'women': [],
-  'kids': [],
-  'customize': [],
-  'sale': [],
-};
-const add_dynamic_dropdown_data = (className) => {
-
-
-
-};
-
-// ==============================================
-
 const navbar = document.querySelector('.nav-desktop');
 const nav_height = element_geometry(navbar).h;
 const nav_dropdown = document.querySelector('.nav-dropdown');
@@ -137,14 +110,79 @@ const slide = (className) => {
 
 // ==============================================
 
-const open = () => {
+const dropdown_data = {
+  'new_releases': [
+    [
+      'New & Featured',
+      'Shop All New Arrivals',
+      'SNKRS Launch Calendar',
+      'Best of Air Max',
+      'Member Access',
+      'Top Picks for Mom',
+      'Mix & Match with Mom'
+    ],
+    [
+      'New For Men',
+      'Shoes',
+      'Clothing',
+      'Equipment',
+      'Shop All New'
+    ],
+    [
+      'New For Women',
+      'Shoes',
+      'Clothing',
+      'Equipment',
+      'Shop All New',
+    ],
+    [
+      'New For Kids',
+      'Boys Shoes',
+      'Boys Clothing',
+      'Girls Shoes',
+      'Girls Clothing',
+      'Shop All New'
+    ]
+  ],
+  'men': [],
+  'women': [],
+  'kids': [],
+  'customize': [],
+  'sale': [],
+};
+const add_dynamic_dropdown_data = (nav_item_id) => {
+  console.log('nav_item_id: ', nav_item_id);
+  const dropdown_data_columns = dropdown_data[nav_item_id];
+  const cols = gsap.utils.toArray('.nav-dropdown__col');
+
+  for (let i = 0; i < dropdown_data_columns.length; i++){
+    console.log('i: ', i);
+    // const elem = document.createElement('div');
+    // console.log(elem);
+    // elem.textContent = dropdown_data_columns[0][0];
+    // col_1.append(elem);
+
+    for (let j = 0; j < dropdown_data_columns[i].length; j++) {
+      console.log('j: ', j);
+
+      const elem = document.createElement('div');
+      elem.textContent = dropdown_data_columns[i][j];
+      cols[i].append(elem);
+
+    }
+  }
+};
+
+// ==============================================
+
+const open = (nav_item_id) => {
   // -currently only used in click listener for
   //  clicking hamburger button in navbar
 
   console.log('clicked hamburger');
 
   // Add dynamic data to dropdown:
-  // add_dynamic_dropdown_data(className);
+  add_dynamic_dropdown_data(nav_item_id);
   
   master_timeline = gsap.timeline();
   master_timeline.add( blur_background() );
@@ -181,6 +219,11 @@ const disable_nav_item_click_listeners = () => {
   // -What we actually want is to only disable the navbar for the currently selected nav-item
   // -This allows user to open another nav-item without clicking outside nav-dropdown to close first.
   // -Should add logic to not re-open the nav-dropdown, but be ready to swap out dynamic data corresponding to different nav-items
+  // -Can do this in function click_listener(event) { event.target.removeEventListener('click', click_listener)}
+  // -Then, don't call disable_nav_item_click_listeners() in the onStart() property of the gsap animatino in slide()
+  // -The issue that I need to work out is how to handle the logic with .reverse() animation [need to not start new animation or do .reverse,
+  //  and instead need to just swap out the dynamic data, which this is currently grabbed in the open() function, which might be a good place to 
+  //  keep this logic since I will call open() when the event of clicking a nav_item (other than the currently selected nav-item) is fired]
 
   // this will run when the dropdown is open to prevent double opens
   nav_items.forEach((nav_item) => {
@@ -195,7 +238,8 @@ const click_listener = (event) => {
   nav_item.classList.add('clicked');
   console.log('nav-item clicked: ', nav_item);
 
-  open();
+  const nav_item_id = nav_item.dataset.id;
+  open(nav_item_id);
 };
 
 // ==============================================
